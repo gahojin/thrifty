5e917f077bcffd725c3491c3c5c8f77eb9a3d166
Zenichi Amano <zenichi.amano@gahojin.co.jp>
2025-02-11 20:08:44 +0900

    build: jacocoからkoverに変更
    Signed-off-by: Zenichi Amano <zenichi.amano@gahojin.co.jp>

--- thrifty-runtime/src/commonTest/kotlin/com/microsoft/thrifty/protocol/Xtruct.kt
+++ thrifty-runtime/src/commonTest/kotlin/com/microsoft/thrifty/protocol/Xtruct.kt
@@ -51,13 +51,12 @@ class Xtruct private constructor(builder: Builder) : Struct {
         if (this === other) return true
         if (other == null) return false
         if (other !is Xtruct) return false
-        val that = other
-        return ((string_thing === that.string_thing || string_thing != null && string_thing == that.string_thing)
-                && (byte_thing === that.byte_thing || byte_thing != null && byte_thing == that.byte_thing)
-                && (i32_thing === that.i32_thing || i32_thing != null && i32_thing == that.i32_thing)
-                && (i64_thing === that.i64_thing || i64_thing != null && i64_thing == that.i64_thing)
-                && (double_thing === that.double_thing || double_thing != null && double_thing == that.double_thing)
-                && (bool_thing === that.bool_thing || bool_thing != null && bool_thing == that.bool_thing))
+        return ((string_thing === other.string_thing || string_thing != null && string_thing == other.string_thing)
+                && (byte_thing == other.byte_thing)
+                && (i32_thing === other.i32_thing || i32_thing != null && i32_thing == other.i32_thing)
+                && (i64_thing === other.i64_thing || i64_thing != null && i64_thing == other.i64_thing)
+                && (double_thing === other.double_thing || double_thing != null && double_thing == other.double_thing)
+                && (bool_thing === other.bool_thing || bool_thing != null && bool_thing == other.bool_thing))
     }
 
     override fun hashCode(): Int {
